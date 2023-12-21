9f83d7e70cd91b346c9086f5009e75546fa8952e
Shivam <shivamsengar995627@gmail.com>
2023-12-21 18:35:46 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useRef, useState } from "react";
+import "./App.css";
 
 function App() {
+  const [isActivated, setIsActivated] = useState(false);
+  const [timer, setTimer] = useState(0);
+  const toggle = () => {
+    setIsActivated((prevState) => !prevState);
+  };
+  let timerId = useRef(null);
+  useEffect(() => {
+    if (isActivated) {
+      timerId.current=setInterval(() => {
+        setTimer((timer) => timer + 1);
+      }, 1000);
+    }
+    return()=>{
+      clearInterval(timerId.current)
+    }
+  }, [timer,isActivated]);
+
+  const formatTheTime=(timer)=>{
+    let minutes=Math.floor(timer / 60)
+    let seconds=timer % 60
+    return `${minutes}:${String(seconds).padStart(2,"0")}`
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ padding: "1rem" }}>
+      <h1>Stopwatch</h1>
+      <div style={{ margin: "40px 0px" }}>Time: {formatTheTime(timer)}</div>
+      <button onClick={toggle}>{isActivated ? "Stop" : "Start"}</button>
+      <button disabled={isActivated===true?true:false} onClick={()=>setTimer(0)}>Reset</button>
     </div>
   );
 }
